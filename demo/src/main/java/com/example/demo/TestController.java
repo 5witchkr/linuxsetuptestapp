a3927002ee9aa1bbfd74967047b9b31b5aaf7364
donghyeon choi <hob4410@gmail.com>
2023-03-20 16:32:56 +0900

    test

--- demo/src/main/java/com/example/demo/TestController.java
+++ demo/src/main/java/com/example/demo/TestController.java
@@ -2,13 +2,26 @@ package com.example.demo;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final TestRepository testRepository;
+
+    public TestController(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
+
     @GetMapping("/test")
-    public String test(){
-       return "TESTTESTTEST!!!";
+    public String gettest(){
+       return "SETSETSETETTETETE";
+    }
+
+    @PostMapping("/test")
+    public String posttest(){
+        testRepository.save(new TestEntity("cosjdfsfd"));
+        return "TESTTESTTEST!!!";
     }
 }
